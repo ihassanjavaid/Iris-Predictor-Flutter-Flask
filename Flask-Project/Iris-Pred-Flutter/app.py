28a91200523c44e7f51c4e744b94b678179a4976
ihassanjavaid <great.maliks@gmail.com>
2020-12-29 00:55:52 +0500

    model_save_update

--- Flask-Project/Iris-Pred-Flutter/app.py
+++ Flask-Project/Iris-Pred-Flutter/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import request, jsonify
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
+# from sklearn import datasets
+# from sklearn.neighbors import KNeighborsClassifier
+from joblib import load
 
 app = Flask(__name__)
 
@@ -31,19 +32,22 @@ def predict():
     # pl = int(petal_length)
     # pw = int(petal_width)
 
-    dataset = datasets.load_iris()
-    x = dataset.data
-    y = dataset.target
+    # dataset = datasets.load_iris()
+    # x = dataset.data
+    # y = dataset.target
+    #
+    # knn = KNeighborsClassifier()
+    # knn.fit(x, y)
 
-    knn = KNeighborsClassifier()
-    knn.fit(x, y)
+    knn = load('iris_model_knn.joblib')
+
     predicted_flower_num = knn.predict([[sl, sw, pl, pw]])[0]
     # predicted_flower_num = knn.predict([[1, 2, 3, 4]])[0]
     predicted_flower = getName(int(predicted_flower_num))
 
     json_file = {}
     json_file['query'] = predicted_flower
-    json_file['flower_number'] = str(predicted_flower_num)
+    # json_file['flower_number'] = str(predicted_flower_num)
     return jsonify(json_file)
 
 
